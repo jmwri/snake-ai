75bc7a032086057fee4a0af98372998eb4c2a8b0
Jim <jim@samknows.com>
2020-02-14 08:40:06 +0000

    Add description to action

--- game/environment/action.py
+++ game/environment/action.py
@@ -2,18 +2,19 @@ from game.vector import Vector
 
 
 class Action:
-    def __init__(self, vector: Vector):
+    def __init__(self, vector: Vector, description: str):
         self.vector = vector
+        self.description = description
 
     def __eq__(self, other: 'Action'):
         return self.vector == other.vector
 
 
-NONE = Action(Vector(0, 0))
-LEFT = Action(Vector(-1, 0))
-UP = Action(Vector(0, -1))
-RIGHT = Action(Vector(1, 0))
-DOWN = Action(Vector(0, 1))
+NONE = Action(Vector(0, 0), "none")
+LEFT = Action(Vector(-1, 0), "left")
+UP = Action(Vector(0, -1), "up")
+RIGHT = Action(Vector(1, 0), "right")
+DOWN = Action(Vector(0, 1), "down")
 
 ALL = (
     LEFT, UP, RIGHT, DOWN,
